a3ff758fcd48ecf39bd24cebba4bfcce4fc5ce3a
Anamika <anamikap6840@gmail.com>
2024-09-03 09:11:55 +0530

    Edit and checkbox completed

--- react_todo/src/component/TodoApp.js
+++ react_todo/src/component/TodoApp.js
@@ -1,51 +1,102 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './TodoApp.css';
 
 export default class TodoApp extends Component {
-  state={
-    input:'',
-    items:[]
-  }
-  handleChange=(event)=>{
-    this.setState({
-      input:event.target.value
-    })
+  state = {
+    input: '',
+    items: [],
+    editingIndex: null,
+    isChecked: []
   };
 
-  storeItem = (event)=>{
-    event.preventDefault()
-    const {input} = this.state
-   
+  handleChange = (event) => {
     this.setState({
-      items:[...this.state.items, input],
-      input:''
+      input: event.target.value
     });
   };
 
-  deleteItem=(key)=>{
-   
-    this.setState({
-      items:this.state.items.filter((data,index)=>index !==key)
-    })
-  }
-  render() {
+  storeItem = (event) => {
+    event.preventDefault();
+    const { input, items, editingIndex, isChecked } = this.state;
 
-    const {input,items} = this.state;
+    if (input.trim() !== '') {
+      if (editingIndex !== null) {
+        const updatedItems = [...items];
+        updatedItems[editingIndex] = input;
+
+        this.setState({
+          items: updatedItems,
+          input: '',
+          editingIndex: null
+        });
+      } else {
+        this.setState({
+          items: [...items, input],
+          input: '',
+          isChecked: [...isChecked, false]
+        });
+      }
+    }
+  };
+
+  deleteItem = (index) => {
+    const { items, isChecked } = this.state;
+    const updatedItems = items.filter((_, i) => i !== index);
+    const updatedChecked = isChecked.filter((_, i) => i !== index);
+
+    this.setState({
+      items: updatedItems,
+      isChecked: updatedChecked
+    });
+  };
+
+  editItem = (index) => {
+    this.setState({
+      input: this.state.items[index],
+      editingIndex: index
+    });
+  };
+
+  handleCheckboxChange = (index) => {
+    const updatedChecked = [...this.state.isChecked];
+    updatedChecked[index] = !updatedChecked[index];
+
+    this.setState({
+      isChecked: updatedChecked
+    });
+  };
+
+  render() {
+    const { input, items, editingIndex, isChecked } = this.state;
     return (
-      <div className='todo-container'>
-         
-         <form className='input-section' onSubmit={this.storeItem}>
-            <h1>Todo App</h1>
-            <input type="text" value={input} onChange={this.handleChange} placeholder='Enter items...' />
-         </form>
-         <ul>
-            {items.map((data,index)=>(
-              <li key={index}>
-                {data} <i className='fas fa-trash-alt' onClick={()=>this.deleteItem(index)}></i>
-              </li>
-            ))}
-         </ul>
+      <div className="todo-container">
+        <form className="input-section" onSubmit={this.storeItem}>
+          <h1>Todo App</h1>
+          <input
+            type="text"
+            value={input}
+            onChange={this.handleChange}
+            placeholder="Enter items..."
+          />
+          <button type="submit">{editingIndex !== null ? 'Save' : 'Add'}</button>
+        </form>
+        <ul>
+          {items.map((data, index) => (
+            <li key={index} className={isChecked[index] ? 'completed' : ''}>
+              <input
+                type="checkbox"
+                checked={isChecked[index]}
+                onChange={() => this.handleCheckboxChange(index)}
+              />
+              {data}
+              <div>
+                <i className="fas fa-edit" onClick={() => this.editItem(index)}></i>
+                <i className="fas fa-trash-alt" onClick={() => this.deleteItem(index)}></i>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
